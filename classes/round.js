d3429fcc4ab18ce855bbe356ae29d812f3e7309b
Polley, Aaron <Aaron.Polley@capitalone.com>
2016-04-05 14:58:12 +0100

    Update fixes

--- classes/round.js
+++ classes/round.js
@@ -220,6 +220,9 @@ class Round{
 		var clientInfo = [];
 		for (var i = this.clients.length - 1; i >= 0; i--) {
 			this.clients[i].update(this.currentLevel.definition.settings.spawnTime, this.world);
+			if(this.clients[i].isDead()){
+				this.resetFlag(this.clients[i]);
+			}
 			clientInfo.push({
 				ID: this.clients[i].ID,
 				Team: this.clients[i].Team,
